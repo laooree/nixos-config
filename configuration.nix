913e500cba81efd8b846558584c3782d8d5087e3
laooree <andrea.lauri96@gmail.com>
2025-06-20 19:09:06 +0200

    Enable XWayland.

--- configuration.nix
+++ configuration.nix
@@ -57,6 +57,7 @@
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
+  programs.xwayland.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
