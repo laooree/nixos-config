4a8915710d4fa8a0c18cab288a45e5c199c900e6
laooree <andrea.lauri96@gmail.com>
2025-07-08 20:55:44 +0200

    Use English locale for datetime.

--- modules/home-manager/i3blocks_scripts/datetime.sh
+++ modules/home-manager/i3blocks_scripts/datetime.sh
@@ -6,6 +6,6 @@ source "$HOME/.config/stylix/colors_env"
 long_date_format="+%A %d %B, %H:%M"
 short_date_format="%H:%M:%S"
 
-echo " 󰃰  $(date "$long_date_format") "
-echo " 󰃰  $(date "$short_date_format") "
+echo " 󰃰  $(LC_TIME=en_US.UTF-8 date "$long_date_format") "
+echo " 󰃰  $(LC_TIME=en_US.UTF-8 date "$short_date_format") "
 echo "${COLOR_BASE06}"
