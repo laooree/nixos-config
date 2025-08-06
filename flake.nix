6b79b0cfca5336f32da5c9d9f0f67ee5028b632d
laooree <andrea.lauri96@gmail.com>
2025-08-06 18:48:57 +0200

    System upgrade + use xremap from older nixpkgs commit.

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-xremap.url = "github:nixos/nixpkgs/7fd36ee82c0275fb545775cc5e4d30542899511d";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -17,7 +18,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-xremap, ... }@inputs:
     {
     nixosConfigurations.sigma = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
@@ -27,6 +28,15 @@
         inputs.home-manager.nixosModules.default
         inputs.xremap-flake.nixosModules.default
         inputs.stylix.nixosModules.stylix
+
+        # Inject an overlay to replace xremap
+        ({ config, pkgs, ... }: {
+          nixpkgs.overlays = [
+            (final: prev: {
+              xremap = nixpkgs-xremap.legacyPackages.${prev.system}.xremap;
+            })
+          ];
+        })
       ];
     };
   };
