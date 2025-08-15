3a91a3bbc5f5e4a3b4b02348ccdf9aa83f6903ce
laooree <andrea.lauri96@gmail.com>
2025-08-15 11:30:12 +0200

    Remove project-specific python libraries.

--- modules/nixos/python.nix
+++ modules/nixos/python.nix
@@ -3,12 +3,7 @@
 {
   environment.systemPackages = with pkgs; [
     (python313.withPackages (python-pkgs: with python-pkgs; [
-      beautifulsoup4
-      matplotlib
       numpy
-      requests
-
-      # needed for get-eduroam.py
-      dbus-python
+      dbus-python  # needed for get-eduroam.py
     ]))];
 }
