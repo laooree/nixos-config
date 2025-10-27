3f3e25039e94bf88308d0c075f00e46347010bbc
laooree <andrea.lauri96@gmail.com>
2025-10-27 21:32:24 +0100

    Bibata cursors.

--- configuration.nix
+++ configuration.nix
@@ -141,6 +141,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    bibata-cursors
     bluetui
     brave
     cava
