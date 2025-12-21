2e90eeebe0af3e1455dc3aa17f110abcd6b6fa37
laooree <andrea.lauri96@gmail.com>
2025-12-21 21:02:37 +0100

    Remove tactile.

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -62,7 +62,6 @@
       gnomeExtensions.paperwm
       gnomeExtensions.dash-to-dock
       gnomeExtensions.user-themes
-      gnomeExtensions.tactile
       gnomeExtensions.just-perfection
 
       # Customization
