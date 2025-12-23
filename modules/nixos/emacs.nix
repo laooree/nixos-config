4bad91f2d4ab5d3f03f949e0181cfb558990a3b9
laooree <andrea.lauri96@gmail.com>
2025-12-23 14:37:44 +0100

    Use stable emacs.

--- modules/nixos/emacs.nix
+++ modules/nixos/emacs.nix
@@ -3,28 +3,7 @@
 {
 
   environment.systemPackages = with pkgs; [
-    (pkgs.emacs.overrideAttrs (old: {
-      name = "emacs-31";
-      version = "31.0.50";
-      src = pkgs.fetchFromGitHub {
-        owner = "emacs-mirror";
-        repo = "emacs";
-        rev = "cf2e676ecab42a3f2add728eca20bb23d2a73728";
-        sha256 = "sha256-9aU1+yzKz9LBxPg2feYQDEb2qNZ6O92aX3GBEKHUoJw=";
-      };
-      buildInputs = old.buildInputs ++ [
-        pkgs.autoconf
-        pkgs.texinfo
-      ];
-      preConfigure = ''
-    ./autogen.sh
-  '';
-      configureFlags = (old.configureFlags or []) ++ [
-        "--prefix=${placeholder "out"}"
-      ];
-    }))
-
-    # emacs
+    emacs
 
     git
     ispell
