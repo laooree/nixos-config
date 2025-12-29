7cb8439e0b3157dbb1cb95e101329d55c26525ec
laooree <andrea.lauri96@gmail.com>
2025-12-29 16:18:14 +0100

    Add nix-switch alias.

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -17,7 +17,8 @@
       ll = "ls -la";
       vi = "nvim";
       vim = "nvim";
-      nix-test = ''sudo nixos-rebuild test --flake "/home/laooree/.dotfiles/"'';
+      nix-test   = ''sudo nixos-rebuild test   --flake "/home/laooree/.dotfiles/"'';
+      nix-switch = ''sudo nixos-rebuild switch --flake "/home/laooree/.dotfiles/"'';
       e = ''emacsclient --no-window --alternate-editor=""'';
       ed =''emacsclient --no-window --eval "(dired \"$(pwd)\")" --alternate-editor=""'';
       pip2nix = "nix run github:nix-community/pip2nix -- generate";
