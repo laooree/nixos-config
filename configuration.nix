1a86887bec8aed5a8a8da9f292b2c40f4e8622b5
laooree <andrea.lauri96@gmail.com>
2026-01-08 17:17:11 +0100

    Dedicated audio module.

--- configuration.nix
+++ configuration.nix
@@ -11,6 +11,7 @@
     inputs.home-manager.nixosModules.default
 
     # custom modules
+    ./modules/nixos/audio.nix
     ./modules/nixos/distrobox.nix
     ./modules/nixos/emacs.nix
     ./modules/nixos/fonts.nix
@@ -80,22 +81,6 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
