e803edebfbb5439b0ac39ff06fcdb0afc3a5c270
laooree <andrea.lauri96@gmail.com>
2026-02-23 22:42:08 +0100

    Build emacs from source with custom derivation.

--- modules/nixos/emacs.nix
+++ modules/nixos/emacs.nix
@@ -1,9 +1,45 @@
 { inputs, config, pkgs, ... }:
 
+let
+
+  emacs-from-source = pkgs.stdenv.mkDerivation {
+    name = "emacs-head";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "emacs-mirror";
+      repo = "emacs";
+      rev = "emacs-30.2";
+      sha256 = "sha256-3Lfb3HqdlXqSnwJfxe7npa4GGR9djldy8bKRpkQCdSA=";
+    };
+
+    buildInputs = with pkgs; [
+      # necessary for minimal build
+      gcc
+      gnumake
+      autoconf
+      texinfo
+      gnutls
+      pkg-config
+      libgccjit
+      ncurses
+      gtk3
+      glib
+    ];
+
+    preConfigure = "./autogen.sh";
+
+    configureFlags = [
+      "--with-native-comp=aot"
+      "--with-pgtk"
+    ];
+  };
+
+in
+
 {
 
   environment.systemPackages = with pkgs; [
-    emacs
+    emacs-from-source
 
     git
     ispell
