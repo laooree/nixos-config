6adf2adc5122b2395452909da5a6b906a274532b
laooree <andrea.lauri96@gmail.com>
2026-05-27 18:29:48 +0200

    System upgrade.

--- modules/nixos/browser.nix
+++ modules/nixos/browser.nix
@@ -12,11 +12,11 @@
   environment.systemPackages = [
 
     (pkgs-unstable.vivaldi.overrideAttrs (oldAttrs: rec {
-    version = "8.0.4033.28";
+    version = "8.0.4033.34";
 
     src = pkgs.fetchurl {
       url = "https://downloads.vivaldi.com/stable/vivaldi-stable_${version}-1_amd64.deb";
-      hash = "sha256-l6XzArL+2tdfVGkBNv+RaziER7KArBR+u8CuS1unBcU=";
+      hash = "sha256-0sQQsiJLStBTzjrd6JRKzrZ/HUZpT68O3tLdLECl7IQ=";
     };
     }))
 
